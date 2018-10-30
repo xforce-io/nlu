e8088b89e14367381898dfb5d83fec207a4765df
freeman <pengxu@emotibot.com>
2018-10-30 08:59:14 +0800

    routine ci

---
+++ segmentor/src/public/req_tracer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "common.h"
+
+namespace xforce { namespace nlu {
+
+class ReqTracer {
+ public: 
+  void set(const std::string &key, const std::string &val)
+
+ private:
+  JsonType *jsonType_;  
+};
+
+}}
