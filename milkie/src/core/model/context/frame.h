d6c7ae3c7e9181ca6a48f6346d6de7db18740d0e
许鹏 <pengxu@emotibot.com>
2019-02-02 21:14:42 +0800

    optimization

--- milkie/src/core/model/context/frame.h
+++ milkie/src/core/model/context/frame.h
@@ -4,7 +4,8 @@
 
 namespace xforce { namespace nlu { namespace milkie {
 
-class StorageItem;  
+class Storage;
+class StorageItem;
 
 class Frame {
  public: 
@@ -13,25 +14,20 @@ class Frame {
 
   inline void SetStartPos(ssize_t startPos);
   ssize_t GetStartPos() const { return startPos_; }
-  inline void SetStorage(const std::wstring &key, const std::wstring &value);
-  inline void SetStorage(const std::wstring &key, StorageItem &newItems);
-  inline void SetStorageStr(const std::wstring &key, const std::wstring &value);
-  inline void RemoveStorage(const std::wstring &key);
-  inline const StorageItem* GetStorage(const std::wstring &key);
-  inline void GetStorages(std::unordered_map<std::wstring, std::wstring> &kvs);
+  const Storage &GetStorage() const { return *storage_; }
+  Storage &GetStorage() { return *storage_; }
   inline void SetStoragePattern(const StorageItem &storageItem);
   inline StorageItem* GetStoragePattern();
-  inline void CopyStorage(const Frame &frame);
-  void DumpStorge();
 
  private:
   ssize_t startPos_;
-  std::unordered_map<std::wstring, StorageItem*> storage_;
+  Storage *storage_;
   StorageItem *storagePattern_;
 };  
 
 }}}
 
+#include "storage.h"
 #include "storage_item.h"
 
 namespace xforce { namespace nlu { namespace milkie {
@@ -41,61 +37,13 @@ Frame::Frame(ssize_t startPos) :
   this->startPos_ = startPos;    
 }
 
+Frame::~Frame() {
+  XFC_DELETE(storagePattern_)
+  XFC_DELETE(storage_)
+}
+
 void Frame::SetStartPos(ssize_t startPos) {
   this->startPos_ = startPos;
-}
-
-void Frame::SetStorage(const std::wstring &key, const std::wstring &value) {
-  auto iter = storage_.find(key);
-  if (iter != storage_.end()) {
-    iter->second->Set(value);
-  } else {
-    storage_.insert(std::make_pair(key, new StorageItem(&value)));
-  }
-}
-
-void Frame::SetStorage(const std::wstring &key, StorageItem &newItems) {
-  StorageItem *storageItem = nullptr;
-  auto iter = storage_.find(key);
-  if (iter == storage_.end()) {
-    storageItem = new StorageItem();
-    storage_.insert(std::make_pair(key, storageItem));
-  } else {
-    storageItem = iter->second;
-  }
-  newItems.Add(*storageItem);
-}
-
-void Frame::SetStorageStr(const std::wstring &key, const std::wstring &value) {
-  auto iter = storage_.find(key);
-  if (iter == storage_.end()) {
-    storage_.insert(std::make_pair(key, new StorageItem(&value)));
-  } else {
-    iter->second->Set(value);
-  }
-}
-
-void Frame::RemoveStorage(const std::wstring &key) {
-  storage_.erase(key);
-}
-
-const StorageItem* Frame::GetStorage(const std::wstring &key) {
-  auto iter = storage_.find(key);
-  if (iter != storage_.end()) {
-    return iter->second;
-  } else {
-    return nullptr;
-  }
-}
-
-void Frame::GetStorages(std::unordered_map<std::wstring, std::wstring> &kvs) {
-  for (auto iter = storage_.begin(); iter != storage_.end(); ++iter) {
-    StorageItem *value = iter->second;
-    const std::wstring *wstr = value->GetAsString();
-    if (nullptr != wstr) {
-      kvs.insert(std::make_pair(iter->first, *wstr));
-    }
-  }
 }
 
 void Frame::SetStoragePattern(const StorageItem &storageItem) {
@@ -107,17 +55,6 @@ void Frame::SetStoragePattern(const StorageItem &storageItem) {
 
 StorageItem* Frame::GetStoragePattern() {
   return storagePattern_;
-}
-
-void Frame::CopyStorage(const Frame &frame) {
-  for (auto iter = frame.storage_.begin(); iter != frame.storage_.end(); ++iter) {
-    auto iter2 = storage_.find(iter->first);
-    if (iter2 != storage_.end()) {
-      iter2->second->Add(*(iter->second));
-    } else {
-      storage_.insert(std::make_pair(iter->first, iter->second));
-    }
-  }
 }
 
 }}}
