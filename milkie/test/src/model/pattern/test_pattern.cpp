7c3fa786e23c05fa37b255d37d50727b7c86351e
许鹏 <pengxu@emotibot.com>
2019-02-08 22:58:06 +0800

    add testcase

--- milkie/test/src/model/pattern/test_pattern.cpp
+++ milkie/test/src/model/pattern/test_pattern.cpp
@@ -22,6 +22,11 @@ TEST(testAll, all) {
   ASSERT_TRUE(ret.first->GetPatternItems().size() == 2);
 
   auto context = std::make_shared<Context>(L"美味的方便面才好吃");
+  Segment::Vector segments;
+  segments.push_back(Segment(Pos::kA, 0, 2));
+  segments.push_back(Segment(Pos::kU, 2, 1));
+  segments.push_back(Segment(Pos::kN, 3, 3));
+  context->GetSentence().GetNluContext()->SetSegments(segments);
   ASSERT_TRUE(ret.first->MatchPattern(*(context.get())));
   ASSERT_TRUE(context->GetCurPos() == 6);
 }
