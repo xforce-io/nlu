cc8d3ed733cf9d2c047d37901c66afac0de07eb5
freeman <pengxu@emotibot.com>
2019-03-01 09:35:27 +0800

    routine ci

---
+++ ner/deps/basic/include/basic/model/fragment/fragment_set_multi.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "fragment_set.h"
+
+namespace xforce { namespace nlu { namespace basic {
+
+class FragmentSetMulti : public FragmentSet {
+
+};
+
+}}}
---
+++ segmentor/deps/basic/include/basic/model/fragment/fragment_set_label.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "fragment_set_non_multi.h"
+
+namespace xforce { namespace nlu { namespace basic {
+
+class FragmentSetLabel : public FragmentSetNonMulti {
+
+};
+
+}}}
