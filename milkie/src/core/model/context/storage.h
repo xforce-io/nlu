8d1b0e3f3a320d2851af02096f4fca840e2502ff
freeman <pengxu@emotibot.com>
2019-03-03 22:34:15 +0800

    routine ci

--- milkie/src/core/model/context/storage.h
+++ milkie/src/core/model/context/storage.h
@@ -7,6 +7,7 @@ namespace xforce { namespace nlu { namespace milkie {
 
 class StorageVal;
 class StorageKey;
+class StorageItem;
 
 class Storage {
  private:
@@ -23,11 +24,17 @@ class Storage {
           HashVal> Container;
 
  public:
-  inline void Set(const StorageKey &key, const std::wstring &val);
+  inline void Set(
+      const StorageKey &key, 
+      const std::wstring &content,
+      size_t offset);
 
   inline void Set(const StorageKey &key, StorageVal &storageItem);
 
-  inline void SetStr(const StorageKey &key, const std::wstring &value);
+  inline void SetStr(
+      const StorageKey &key, 
+      const std::wstring &content,
+      size_t offset);
 
   inline void Remove(const StorageKey &key);
 
@@ -35,7 +42,7 @@ class Storage {
 
   inline const Container& Get() const;
 
-  inline void GetStrs(std::unordered_map<std::wstring, std::wstring> &kvs);
+  inline void GetStrs(std::unordered_map<std::wstring, StorageItem> &kvs);
 
   inline void Merge(const Storage &storage);
 
@@ -49,10 +56,13 @@ class Storage {
 
 namespace xforce { namespace nlu { namespace milkie {
 
-void Storage::Set(const StorageKey &key, const std::wstring &value) {
+void Storage::Set(
+    const StorageKey &key, 
+    const std::wstring &content,
+    size_t offset) {
   auto iter = container_.find(key);
   if (iter != container_.end()) {
-    iter->second->Set(value);
+    iter->second->Set(content, offset);
   }
 }
 
@@ -68,12 +78,15 @@ void Storage::Set(const StorageKey &key, StorageVal &newItems) {
   storageItem->Add(newItems);
 }
 
-void Storage::SetStr(const StorageKey &key, const std::wstring &value) {
+void Storage::SetStr(
+    const StorageKey &key, 
+    const std::wstring &content,
+    size_t offset) {
   auto iter = container_.find(key);
   if (iter == container_.end()) {
-    container_.insert(std::make_pair(key, new StorageVal(&value)));
+    container_.insert(std::make_pair(key, new StorageVal(content, offset)));
   } else {
-    iter->second->Set(value);
+    iter->second->Set(content, offset);
   }
 }
 
@@ -94,7 +107,7 @@ const Storage::Container& Storage::Get() const {
   return container_;
 }
 
-void Storage::GetStrs(std::unordered_map<std::wstring, std::wstring> &kvs) {
+void Storage::GetStrs(std::unordered_map<std::wstring, StorageItem> &kvs) {
   kvs.clear();
   for (auto iter = container_.begin(); iter != container_.end(); ++iter) {
     auto value = iter->second->GetAsString();
