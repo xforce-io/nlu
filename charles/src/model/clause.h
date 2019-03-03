c9a788f59b7642c525de212b0336c486d03d130a
许鹏 <pengxu@emotibot.com>
2019-03-03 11:06:07 +0800

    routine ci

--- charles/src/model/clause.h
+++ charles/src/model/clause.h
@@ -4,25 +4,16 @@
 
 namespace xforce { namespace nlu { namespace charles {
 
-class FeatureSegments;
-class FeatureNameEntity;
-
 class Clause {
  private:
-  typedef std::list<FeatureNameEntity*> FeatureNameEntities; 
 
  public:
   Clause(const std::wstring &clause);
-
-  void Segment(); 
 
   virtual ~Clause();
 
  private:
   std::wstring clause_;
-
-  FeatureSegments *featureSegments_;
-  FeatureNameEntities featureNameEntities_;
 };
 
 }}}
--- charles/src/model/details/clause.cpp
+++ charles/src/model/details/clause.cpp
@@ -4,25 +4,9 @@
 namespace xforce { namespace nlu { namespace charles {
 
 Clause::Clause(const std::wstring &clause) :
-  clause_(clause),
-  featureSegments_(NULL) {}
-
-void Clause::Segment() {
-  basic::Segment::Vector segments;
-  std::vector<std::shared_ptr<ner::NameEntity>> nameEntities;
-  BaseModules::Get().GetSegmentor().Parse(clause_, segments, nameEntities);
-
-  featureSegments_ = new FeatureSegments(clause_, segments);
-  for (auto iter = nameEntities.begin(); iter != nameEntities.end(); ++iter) {
-    featureNameEntities_.push_back(new FeatureNameEntity(*featureSegments_, *iter, 0));
-  }
-}
+  clause_(clause) {}
 
 Clause::~Clause() {
-  for (auto iter = featureNameEntities_.begin(); iter != featureNameEntities_.end(); ++iter) {
-    XFC_DELETE(*iter)
-  }
-  XFC_DELETE(featureSegments_)
 }
 
 }}}
