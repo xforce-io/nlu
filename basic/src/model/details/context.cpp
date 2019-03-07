4c5fc1b92bdf18b5cdd3d67fdfab5bff42a4b7b6
许鹏 <pengxu@emotibot.com>
2019-03-07 23:18:50 +0800

    add gkb

--- basic/src/model/details/context.cpp
+++ basic/src/model/details/context.cpp
@@ -3,7 +3,9 @@
 namespace xforce { namespace nlu { namespace basic {
 
 Context::Context(const std::wstring &query) :
-  query_(query) {}
+  query_(query),
+  segments_(nullptr),
+  nameEntities_(nullptr) {}
 
 Context::~Context() {
   XFC_DELETE(nameEntities_)
