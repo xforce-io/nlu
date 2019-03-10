c6635d05df98dd4c774e8c907a1ea0dfd01f7dc4
freeman <pengxu@emotibot.com>
2019-03-10 22:56:49 +0800

    compile error fix

---
+++ charles/deps/basic/include/basic/model/context.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../public.h"
+#include "fragment/fragment_set.hpp"
+
+namespace xforce { namespace nlu { namespace basic {
+
+class Segment;
+class NameEntity;
+
+class Context {
+ public:
+  Context(const std::wstring &query);
+  virtual ~Context();
+
+  const FragmentSet<Segment>& GetSegments() const { return *segments_; }
+  FragmentSet<Segment>& GetSegments() { return *segments_; }
+  const FragmentSet<NameEntity>& GetNameEntities() const { return *nameEntities_; }
+  FragmentSet<NameEntity>& GetNameEntities() { return *nameEntities_; }
+
+ private:
+  std::wstring query_;
+  FragmentSet<Segment> *segments_;
+  FragmentSet<NameEntity> *nameEntities_;
+};
+
+}}}
