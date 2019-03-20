75ab5e92c83cf3e166c45593a498c0818c040c97
freeman <pengxu@emotibot.com>
2019-03-20 10:24:02 +0800

    routine ci

---
+++ milkie/deps/basic/include/basic/model/syntax/syntax_tag.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "../../public.h"
+
+namespace xforce { namespace nlu { namespace basic {
+
+class SyntaxTag {
+ public:
+  enum Type {
+      kAdjp, //Adjective phrase / 形容词短语
+      kAdvp, //Adverbial phrase headed by AD (adverb) / 由副词开头的副词短语
+      kClp, //Classifier phrase / 量词短语
+      kCp, //Clause headed by C (complementizer) / 由补语引导的补语从句
+      kDnp, //Phrase formed by “XP+DEG”
+      kDp, //Determiner phrase / 限定词短语
+      kDvp, //Phrase formed by “XP+DEV”
+      kFrag, //fragment
+      kIp, //Simple clause headed by I (INFL 或其曲折成分)
+      kLcp, //Phrase formed by “XP+LC” / LC位置词
+      kLst, //List marker / 列表标记，如“--”
+      kNp, //Noun phrase / 名词短语
+      kPp, //Preposition phrase / 介词短语
+      kPrn, //Parenthetical / 括号中的，插入的
+      kQp, //Quantifier phrase / 量词短语
+      kUcp, //unidentical coordination phrase / 非对等同位语短语
+      kVp, //Verb phrase / 动词短语
+      kUndef,
+  };
+
+ public:
+  static SyntaxTag::Type GetSyntaxTag(const std::wstring &syntaxTag);
+  static const std::wstring& Str(SyntaxTag::Type type);
+};
+
+}}}
