060f3fce4d132b3c16e8a6f3ac5599e340ead703
许鹏 <pengxu@emotibot.com>
2019-04-09 09:46:37 +0800

    routine ci

--- pos_tagging/test/src/test_case.cpp
+++ pos_tagging/test/src/test_case.cpp
@@ -20,4 +20,15 @@ int main(int argc, char **argv) {
 TEST(test_case, all) {
   const xforce::JsonType* conf = xforce::JsonType::CreateConf("conf/pos.conf");
   ASSERT_TRUE(PosTagging::Init((*conf)["pos"]));
+
+  std::wstring query = L"这真的不是太好吧";
+  basic::NluContext nluContext(query);
+  nluContext.GetSegments().Add(Segment(PosTag::kUndef, 0, 1));
+  nluContext.GetSegments().Add(Segment(PosTag::kUndef, 1, 2));
+  nluContext.GetSegments().Add(Segment(PosTag::kUndef, 3, 1));
+  nluContext.GetSegments().Add(Segment(PosTag::kUndef, 4, 1));
+  nluContext.GetSegments().Add(Segment(PosTag::kUndef, 5, 2));
+  nluContext.GetSegments().Add(Segment(PosTag::kUndef, 7, 1));
+
+  PosTagging::Tagging(nluContext);
 }
