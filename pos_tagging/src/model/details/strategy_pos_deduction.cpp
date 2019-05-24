ac6c70feff345f4cb05c79a93cd6304247449b0c
许鹏 <pengxu@emotibot.com>
2019-05-24 14:14:45 +0800

    routine ci

--- pos_tagging/src/model/details/strategy_pos_deduction.cpp
+++ pos_tagging/src/model/details/strategy_pos_deduction.cpp
@@ -6,7 +6,7 @@ void StrategyPosDeduction::Process(basic::NluContext &nluContext) {
   auto &clause = nluContext.GetQuery();
   typename basic::Segment::Set &segments = nluContext.GetSegments();
   for (size_t i=0; i < segments.Size(); ++i) {
-
+    ProcessAdverb_(segments, i);
   }
 }
 
@@ -18,8 +18,10 @@ void StrategyPosDeduction::ProcessAdverb_(
   }
 
   auto &cur = segments[idx];
-  if (cur->SizePosTags() == 1 &&
-      cur->GetPosTag() == basic::PosTag::kD) {
+  if ((cur->SizePosTags() == 1 &&
+          cur->GetPosTag() == basic::PosTag::kD) ||
+      (cur->SizePosTags() > 1 &&
+          cur->ContainPosTag(basic::PosTag::kD))) {
     bool hasPredAfterward = false;
     for (size_t i = idx+2; i < segments.Size(); ++i) {
       for (auto &posTag : segments[i]->GetPosTags()) {
@@ -34,15 +36,28 @@ void StrategyPosDeduction::ProcessAdverb_(
       }
     }
 
-    if (!hasPredAfterward) {
+    if (hasPredAfterward) {
       return;
     }
 
-    auto &next = segments[idx+1];
-
-  } else if (cur->SizePosTags() > 1 &&
-      cur->ContainPosTag(basic::PosTag::kD)) {
-
+    if (cur->SizePosTags() == 1) {
+      auto &next = segments[idx+1];
+      if (next->SizePosTags() > 1) {
+        for (auto &posTag : next->GetPosTags()) {
+          if (posTag != basic::PosTag::kV &&
+              posTag != basic::PosTag::kA) {
+            next->RemovePosTag(posTag);
+          }
+        }
+      }
+    } else {
+      auto &next = segments[idx+1];
+      if (next->SizePosTags() == 1 &&
+          next->GetPosTag() != basic::PosTag::kV &&
+          next->GetPosTag() != basic::PosTag::kA) {
+        cur->RemovePosTag(basic::PosTag::kD);
+      }
+    }
   }
 }
 
