46cbdc1e8752b875bc13152db42c04c2e4268272
许鹏 <pengxu@emotibot.com>
2019-05-26 11:02:19 +0800

    routine ci

--- basic/src/data/gkb/details/gkb.cpp
+++ basic/src/data/gkb/details/gkb.cpp
@@ -1,21 +1,31 @@
 #include "../gkb.h"
+#include "../table/gkb_global.h"
 
 namespace xforce { namespace nlu { namespace basic {
 
-Gkb::Gkb()
-  : gkbAdv_(nullptr) {}
+Gkb::Gkb() :
+  gkbGlobal_(nullptr),
+  gkbAdv_(nullptr) {}
 
 Gkb::~Gkb() {
   XFC_DELETE(gkbAdv_)
+  XFC_DELETE(gkbGlobal_)
 }
 
 bool Gkb::Init(const std::string &dir) {
   std::stringstream ss;
   ss << dir << "/gkb_global";
+  gkbGlobal_ = new GkbGlobal();
+  bool ret = gkbGlobal_->Init(ss.str());
+  if (!ret) {
+    FATAL("fail_init_gkb_global");
+    return false;
+  }
 
+  ss.str("");
+  ss << dir << "/gkb_adv";
   gkbAdv_ = new GkbAdv();
-
-  bool ret = gkbAdv_->Init(ss.str());
+  ret = gkbAdv_->Init(ss.str());
   if (!ret) {
     FATAL("fail_init_gkb_adv");
     return false;
--- basic/src/data/gkb/gkb.h
+++ basic/src/data/gkb/gkb.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../../public.h"
+#include "table/gkb_global.h"
 #include "table/gkb_adv.h"
 
 namespace xforce { namespace nlu { namespace basic {
@@ -12,9 +13,11 @@ class Gkb {
 
   bool Init(const std::string &dir);
 
+  const GkbGlobal& GetGkbGlobal() const { return *gkbGlobal_; }
   const GkbAdv& GetGkbAdv() const { return *gkbAdv_; }
 
  private:
+  GkbGlobal *gkbGlobal_;
   GkbAdv *gkbAdv_;
 };
 
