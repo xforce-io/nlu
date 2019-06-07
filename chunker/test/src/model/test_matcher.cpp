510db1df887295adf3edfb349893a4a55efddea1
许鹏 <pengxu@emotibot.com>
2019-06-07 15:59:29 +0800

    routine ci

--- chunker/test/src/model/test_matcher.cpp
+++ chunker/test/src/model/test_matcher.cpp
@@ -19,7 +19,7 @@ using namespace xforce::nlu::chunker;
 
 int main(int argc, char **argv) {
   setlocale(LC_ALL, "");
-  LOGGER_SYS_INIT(L"../../conf/log.conf");
+  LOGGER_SYS_INIT(L"../conf/log.conf");
 
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
