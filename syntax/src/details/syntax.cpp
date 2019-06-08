3f8daaa40e2f04e15c5193798aa55e723b6ea598
许鹏 <pengxu@emotibot.com>
2019-06-08 23:53:34 +0800

    routine ci

---
+++ syntax/src/details/syntax.cpp
@@ -0,0 +1,47 @@
+#include "../syntax.h"
+#include "../model/matcher.h"
+#include "../conf/conf.h"
+
+namespace xforce { namespace nlu { namespace syntax {
+
+Syntax::Syntax() :
+  matcher_(new Matcher()) {}
+
+Syntax::~Syntax() {
+  XFC_DELETE(matcher_)
+}
+
+bool Syntax::Init() {
+  auto ret = matcher_->Init();
+  if (!ret) {
+    FATAL("fail_init_milkie");
+    return false;
+  }
+  return true;
+}
+
+void Syntax::Process(basic::NluContext &nluContext) {
+  matcher_->Match(nluContext);
+}
+
+bool Syntax::Init(const xforce::JsonType &confJson) {
+  bool ret = Conf::Get().Init(confJson);
+  if (!ret) {
+    FATAL("fail_init_conf[syntax]");
+    return false;
+  }
+
+  ret = syntax_.Init();
+  if (!ret) {
+    FATAL("fail_init_syntax");
+    return false;
+  }
+  std::cout << "succ init pos" << std::endl;
+  return true;
+}
+
+void Syntax::Parse(basic::NluContext &nluContext) {
+  return syntax_.Process(nluContext);
+}
+
+}}}
---
+++ syntax/src/syntax.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "public.h"
+
+namespace xforce { namespace nlu { namespace syntax {
+
+class Matcher;
+
+class Syntax {
+ public:
+  Syntax();
+  virtual ~Syntax();
+
+  bool Init();
+  void Process(basic::NluContext &nluContext);
+
+ public: 
+  static bool Init(const xforce::JsonType &confJson);
+  static void Parse(basic::NluContext &nluContext);
+  static void Tini() {}
+
+ private:
+  Matcher *matcher_;
+
+  static Syntax syntax_;
+};
+
+}}}
