39c0f1c732c00df461a080f24062cd1260cf70a4
许鹏 <pengxu@emotibot.com>
2019-06-11 09:57:37 +0800

    routine ci

--- pos_tagging/src/model/details/strategy_complement.cpp
+++ pos_tagging/src/model/details/strategy_complement.cpp
@@ -5,15 +5,14 @@ namespace xforce { namespace nlu { namespace pos {
 void StrategyComplement::Process(basic::NluContext &nluContext) {
   auto &clause = nluContext.GetQuery();
   typename basic::Segment::Set &segments = nluContext.GetSegments();
-  for (size_t i=0; i < segments.Size(); ++i) {
-    auto &segment = segments[i];
+  for (auto segment : segments.GetAll())
     if (segment->GetPosTags().empty()) {
       auto segmentStr = segment->GetStrFromSentence(clause);
       auto poses = basic::Manager::Get().GetGkb().GetGkbGlobal().GetPosTags(segmentStr);
       if (poses != nullptr && poses->size() >= 1) {
-        SetPos(segment, (*poses)[0], kStrategyOther);
+        SetPos(*segment, (*poses)[0], kStrategyOther);
         for (size_t i=1; i < poses->size(); ++i) {
-          AddPos(segment, (*poses)[i]);
+          AddPos(*segment, (*poses)[i]);
         }
       }
     }
