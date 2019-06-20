0f96c4127b82b6ba2dd9afcd431401871087277a
许鹏 <pengxu@emotibot.com>
2019-06-20 18:33:21 +0800

    routine ci

--- milkie/src/core/model/pattern_item/details/pattern_item_syntax.cpp
+++ milkie/src/core/model/pattern_item/details/pattern_item_syntax.cpp
@@ -7,19 +7,31 @@ PatternItemSyntax::PatternItemSyntax(const basic::SyntaxTag::Type &syntaxType) :
   syntaxType_(syntaxType) {}
 
 bool PatternItemSyntax::MatchPattern(Context &context) {
-  auto featureChunk = context.GetSentence().GetFeatureChunkAtOffset(context.GetCurPos());
-  if (nullptr == featureChunk) {
+  auto chunkSet = context.GetSentence().GetFeatureChunkAtOffset(context.GetCurPos());
+  if (nullptr == chunkSet) {
     return false;
   }
 
-  if (featureChunk->GetSyntaxTag() != syntaxType_) {
-    return false;
+  std::shared_ptr<basic::Chunk> theChunk = nullptr;
+  size_t maxLen = 0;
+  for (auto &chunk : chunkSet->GetAll()) {
+    if (chunk->GetSyntaxTag() != syntaxType_) {
+      continue;
+    }
+
+    if (chunk->GetLen() > maxLen) {
+      theChunk = chunk;
+      maxLen = chunk->GetLen();
+    }
   }
 
-  contentMatched_ = context.GetSentence().GetSentence().substr(
-          featureChunk->GetOffset(),
-          featureChunk->GetLen());
-  return true;
+  if (nullptr != theChunk) {
+    contentMatched_ = context.GetSentence().GetSentence().substr(
+            theChunk->GetOffset(),
+            theChunk->GetLen());
+    return true;
+  }
+  return false;
 }
 
 
