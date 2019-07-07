0677255a21cc542c12ca73e5fc19909ddc987dd6
许鹏 <pengxu@emotibot.com>
2019-07-07 23:39:34 +0800

    routine ci

---
+++ pos_tagging/src/model/details/strategy_special_token.cpp
@@ -0,0 +1,24 @@
+#include "../strategy_special_token.h"
+
+namespace xforce { namespace nlu { namespace pos {
+
+void StrategySpecialToken::Process(basic::NluContext &nluContext) {
+  for (auto &segment : nluContext.GetSegments().GetAll()) {
+    if (!segment->GetTags().empty()) {
+      continue;
+    }
+
+    auto &segStr = segment->GetStrFromSentence(nluContext.GetQuery());
+    for (size_t i=0; i < segStr.length(); ++i) {
+      wchar_t c = segStr[i];
+      bool isLetter = (c >= L'a' && c <= L'z') || (c >= L'A' && c <= L'Z');
+      bool isNum = (c >= L'0' && c <= L'9');
+      if (isLetter || isNum) {
+        segment->AddTag(basic::PosTag::Type::kN);
+        break;
+      }
+    }
+  }
+}
+
+}}}
