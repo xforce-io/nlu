53da3a90c4d2d74b12ef9ef67142954308bf4f43
freeman <pengxu@emotibot.com>
2019-07-09 21:52:11 +0800

    routine ci

--- pos_tagging/deps/basic/include/basic/public.h
+++ pos_tagging/deps/basic/include/basic/public.h
@@ -1,3 +1,18 @@
 #pragma once
 
 #include "public/public.h"
+
+namespace xforce { namespace nlu { namespace basic {
+
+struct Stage {
+  enum Type {
+    kNone,
+    kSegment,
+    kPosTag,
+    kChunk,
+    kSyntax,
+    kEnd,
+  };
+};
+
+}}}
