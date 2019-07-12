9df306dc2ec82194c6cfdb50339a0a5dbc8e00a4
许鹏 <pengxu@emotibot.com>
2019-07-12 09:21:49 +0800

    routine ci

--- charles/src/model/process/processor_clause.h
+++ charles/src/model/process/processor_clause.h
@@ -6,8 +6,15 @@ namespace xforce { namespace nlu { namespace charles {
 
 class AnalysisContext;
 class AnalysisClause;
+class AnalysisSentence;
 
 class ProcessorClause {
+ public:
+  //@return : is analysisSentence modified
+  typedef bool (*Processor)(
+          AnalysisContext &analysisContext,
+          AnalysisSentence &analysisSentence);
+
  public:
   inline static bool Process(
       AnalysisContext &analysisContext,
