80cf49bf7da819d0b43c8bba67e7b6baf5c996f4
许鹏 <pengxu@emotibot.com>
2019-07-26 22:26:24 +0800

    routine ci

--- pos_tagging/src/model/details/strategy_complement.cpp
+++ pos_tagging/src/model/details/strategy_complement.cpp
@@ -11,6 +11,10 @@ void StrategyComplement::Process(basic::NluContext &nluContext) {
       auto poses = basic::Manager::Get().GetGkb().GetGkbGlobal().GetPosTags(segmentStr);
       if (poses != nullptr && poses->size() >= 1) {
         SetPos(*segment, (*poses)[0], kStrategyOther);
+        if ((*poses)[0] == basic::PosTag::Type::kV) {
+          AddPos(*segment, basic::PosTag::Type::kVn);
+        }
+
         for (size_t i=1; i < poses->size(); ++i) {
           AddPos(*segment, (*poses)[i]);
           if ((*poses)[i] == basic::PosTag::Type::kV) {
