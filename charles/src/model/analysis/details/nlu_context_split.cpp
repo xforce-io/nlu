92a49e31100c1d55b98aad81e1ad0f7f00725b40
许鹏 <pengxu@emotibot.com>
2019-08-23 23:18:59 +0800

    routine ci

--- charles/src/model/analysis/details/nlu_context_split.cpp
+++ charles/src/model/analysis/details/nlu_context_split.cpp
@@ -3,9 +3,9 @@
 
 namespace xforce { namespace nlu { namespace charles {
 
-const std::wstring NluContextSplit::kBranch0SyntaxStoragePrefix = L"/branch0-syntactic";
-const std::wstring NluContextSplit::kBranch1SyntaxStoragePrefix = L"/branch1-syntactic";
-const std::wstring NluContextSplit::kBranch2SyntaxStoragePrefix = L"/branch2-syntactic";
+const std::wstring NluContextSplit::kBranch0SyntaxStoragePrefix = L"/branch0_syntactic";
+const std::wstring NluContextSplit::kBranch1SyntaxStoragePrefix = L"/branch1_syntactic";
+const std::wstring NluContextSplit::kBranch2SyntaxStoragePrefix = L"/branch2_syntactic";
 
 NluContextSplit::NluContextSplit() :
   splitRuleEngine_(nullptr) {}
@@ -122,9 +122,11 @@ bool NluContextSplit::SplitBySyntax_(
     auto storageItems = storageKv.second->Get();
     for (auto &storageItem : storageItems) {
       basic::Chunk chunk(
+              *nluContext,
               syntaxTag,
               storageItem.GetOffset(),
-              storageItem.GetContent().length());
+              storageItem.GetContent().length(),
+              970);
       if (branches[index]->GetChunks().Add(chunk)) {
         if (basic::SyntaxTag::Type::kStc == syntaxTag) {
           return true;
