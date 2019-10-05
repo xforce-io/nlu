4356105df72054738cc657aaec42fa08bf803306
freeman <freeman@freemans-MacBook-Pro.local>
2019-10-05 21:18:18 +0800

    routine ci

---
+++ basic/src/model/analysis_tracer/details/analysis_tracer.cpp
@@ -0,0 +1,25 @@
+#include "../analysis_tracer.h"
+
+namespace xforce { namespace nlu { namespace basic {
+
+void AnalysisTracer::AddEvent(const std::wstring &event) {
+  Get()->Add("event", event);
+}
+
+AnalysisTracer* AnalysisTracer::Get() {
+  auto analysisTracer = (AnalysisTracer*)Tracer::threadPrivacy_->Get(kNoAnalysisTracer);
+  if (nullptr == analysisTracer) {
+    auto newAnalysisTracer = new AnalysisTracer();
+    analysisTracer = Tracer::threadPrivacy_->Get(
+            kNoAnalysisTracer,
+            newAnalysisTracer,
+            nullptr);
+    if (nullptr == analysisTracer) {
+      XFC_DELETE(newAnalysisTracer)
+      return nullptr;
+    }
+  }
+  return analysisTracer;
+}
+
+}}}
