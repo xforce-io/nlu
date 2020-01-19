a64d142a977f7375177e4694ffd914f476129d7b
freemanXu <superxup@gmail.com>
2020-01-19 20:05:16 +0800

    routine ci

---
+++ charles/test/src/model/analysis/split/test_rule_syntax_prep.cpp
@@ -0,0 +1,31 @@
+#include <locale>
+
+#include "gtest/gtest.h"
+
+#include "../../../../../src/model/analysis/analysis_clause.h"
+#include "../../../../../src/charles.h"
+
+LOGGER_IMPL(xforce::xforce_logger, L"charles")
+
+using namespace xforce;
+using namespace xforce::nlu;
+using namespace xforce::nlu::charles;
+
+int main(int argc, char **argv) {
+  setlocale(LC_ALL, "");
+  LOGGER_SYS_INIT(L"../conf/log.conf");
+
+  testing::InitGoogleTest(&argc, argv)
+  return RUN_ALL_TESTS();
+}
+
+TEST(testAll, all) {
+  const xforce::JsonType* conf = xforce::JsonType::CreateConf("../conf/charles.conf");
+
+  ASSERT_TRUE(Charles::Init(*conf));
+  AnalysisClause analysisClause(L"不能说为了跟意大利比赛");
+  ASSERT_TRUE(analysisClause.Init());
+  analysisClause.Process();
+
+  std::cout << basic::AnalysisTracer::Get()->GetReport() << std::endl;
+}
