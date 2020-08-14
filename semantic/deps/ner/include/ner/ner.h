a85017448a85953f2f41fab8b5e0309b12693a77
freeman <pengxu@emotibot.com>
2020-08-14 20:03:26 +0800

    routine ci

--- semantic/deps/ner/include/ner/ner.h
+++ semantic/deps/ner/include/ner/ner.h
@@ -5,8 +5,16 @@
 namespace xforce { namespace nlu { namespace ner {
 
 class Ner {
+ public:
+  Ner();
+  virtual ~Ner();
+
+  bool Init();
+  void Process(std::shared_ptr<basic::NluContext> nluContext);
+
  public: 
-  static bool Init(const xforce::JsonType &confJson); 
+  static bool Init(const xforce::JsonType &confJson);
+  static void Parse(std::shared_ptr<basic::NluContext> nluContext);
 
   static void Tini();
 };
